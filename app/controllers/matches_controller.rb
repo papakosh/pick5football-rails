7de867718c5d0ae77a329459586a16269a83a213
papakosh <brian.navarro@gmail.com>
2015-12-29 09:18:37 -0600

    Added week17

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -19,6 +19,6 @@ class MatchesController < ApplicationController
 	WEEKS = ["Week1", "Week2", "Week3", "Week4",
 			"Week5", "Week6", "Week7", "Week8",
 			"Week9", "Week10", "Week11", "Week12",
-			"Week13", "Week14", "Week15", "Week16"]
+			"Week13", "Week14", "Week15", "Week16", "Week17"]
 	YEARS = ["2015", "2016", "2017"]
 end
